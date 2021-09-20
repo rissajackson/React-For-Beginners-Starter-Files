f5b7b67fae13ed2e6a5da6ead13ad79cd90cb460
Rissa Jackson <57572484+rissajackson@users.noreply.github.com>
2021-09-20 18:03:53 -0500

    Add button to storepicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,10 +2,16 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    handleClick() {
+        alert("Heyyy!")
+    }
     render() {
         return (
             <form className="store-selector">
                 <h2>Please Enter A Store</h2>
+
+                <button onClick={this.handleClick}>Click me!</button>
+
                 <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store -></button>
             </form>
