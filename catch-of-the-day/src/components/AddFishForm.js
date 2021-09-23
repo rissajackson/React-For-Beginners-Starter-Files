c508e54dc12abf66706abb4a5284a3ca412de92a
Rissa Jackson <57572484+rissajackson@users.noreply.github.com>
2021-09-23 18:06:43 -0500

    Add fields for add fish form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,8 +3,12 @@ import React from "react";
 class AddFishForm extends React.Component {
     render() {
         return (
-            <form>
-                <p>It Works!</p>
+            <form className="fish-edit">
+                <input name="name" type="text" placeholder="Name" />
+                <input name="price" type="text" placeholder="Price" />
+                <input name="status" type="text" placeholder="Status" />
+                <input name="desc" type="text" placeholder="Desc" />
+                <input name="image" type="text" placeholder="Image" />
             </form>
         );
     }
